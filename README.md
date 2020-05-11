cebbaff670f71dc749c1e50d0cfb20b567dc71fe
Ferenc Almasi <v-falmasi@hotels.com>
2020-05-11 18:13:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">
+    <img src="hero.jpg" alt="Drag and drop functionality created in vanilla JavaScript" />
+</h1>
+<h4 align="center">You can read the written tutorial about the implementation on <strong><a href="https://medium.com/@ferencalmasi">Medium</a></strong> 📌</h4>
---
+++ drag-n-drop.js
@@ -0,0 +1,43 @@
+const dragStart = event => {
+    event.currentTarget.classList.add('dragging');
+};
+
+const dragEnd = event => {
+    event.currentTarget.classList.remove('dragging');
+};
+
+const dragEnter = event => {
+    event.currentTarget.classList.add('drop');
+};
+
+const dragLeave = event => {
+    event.currentTarget.classList.remove('drop');
+};
+
+const drag = event => {
+    event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
+    event.dataTransfer.setData('text/plain', event.currentTarget.dataset.id);
+};
+
+const drop = event => {
+    Array.from(document.querySelectorAll('.column'))
+         .forEach(column => column.classList.remove('drop'));
+
+    document.querySelector(`[data-id="${event.dataTransfer.getData('text/plain')}"]`).remove();
+
+    event.currentTarget.innerHTML = event.currentTarget.innerHTML + event.dataTransfer.getData('text/html');
+};
+
+const allowDrop = event => {
+    event.preventDefault();
+};
+
+Array.from(document.querySelectorAll('.card')).forEach(card => {
+    card.addEventListener('dragstart', dragStart);
+    card.addEventListener('dragend', dragEnd);
+});
+
+Array.from(document.querySelectorAll('.column')).forEach(column => {
+    column.addEventListener('dragenter', dragEnter);
+    column.addEventListener('dragleave', dragLeave);
+});
